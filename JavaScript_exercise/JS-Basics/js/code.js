e73d760a1bb979ad9fdfe69317cb65c1109979d3
Dan1elH3ctor <danielhector080@gmail.com>
2022-11-09 15:03:55 +0200

    update

---
+++ JavaScript_exercise/JS-Basics/js/code.js
@@ -0,0 +1,55 @@
+function myFunction() {
+    alert("Hello! My name is Daniel Hector!!");
+    }
+    // array
+    // let students = [
+    //     'Gift', 'Asandile',
+    //     'Deno', 'Javier',
+    //     'Sakhe', 'Mkhuseli',
+    //     'John', 'Thando', `Daniel`
+    // ]
+    // console.log(students.length);
+    // console.log(students)
+    // console.log(students[4])
+
+//  comparison operators
+// let numb1 = 6;
+// let numb2 = 5;
+// console.log(numb1 <= numb2)
+// console.log(numb1 >= numb2)
+// console.log(numb1 != numb2)
+// console.log(numb1 == numb2)
+// let salary  = parseFloat(prompt("Enter your salary: "));
+// let age = parseInt(prompt("enter your age: "));
+// let qualified = (salary >= 5000) &&
+// let qualified = (salary >= 5000) ||
+// (age > 17);
+// document.write(qualified);
+// console.log(qualified);
+
+// tenuary operator
+// let age = 17;
+// console.log(age > 17 ? "You are qualified" : "You are not qualified");
+
+// increment
+// let numb1 = 8;
+// console.log(numb1++);
+// console.log(numb1);
+
+// Decrement
+// let numb1 = 8;
+// console.log(numb1--);
+// console.log(numb1);
+
+// task
+
+// let username = parseFloat(prompt("Enter your name"));
+// let salary = parseInt(prompt("enter your salary"))
+// let bonus = 1000;
+// let qualified = (salary > 5000) &&
+// (salary > 5000);
+// console.log(salary++);
+// console.log(bonus)
+
+// const
+const numb1 = 
