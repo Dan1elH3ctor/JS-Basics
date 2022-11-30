bb6af3e8e3f1b3e0cde6e0b6210af758b71e00f9
Dan1elH3ctor <danielhector080@gmail.com>
2022-11-30 15:46:05 +0200

    update

--- JavaScript_exercise/JS_revision/js/code.js
+++ JavaScript_exercise/JS_revision/js/code.js
@@ -47,7 +47,11 @@ console.log("includes(are):", sentence.includes("are"));
 console.log(sentence.split('e'));
 
 // date method
-
+let myDate = new Date();
+console.log("date:", myDate.toLocaleDateString());
+let currentDate = myDate.getDate();
+console.log("setDate(): ", new Date(myDate.setDate(currentDate + 5)));
+// date format
 
 // if statement
 let age = 17;
@@ -57,3 +61,71 @@ if(age>17) {
 else{
     console.log("you are not qualified")
 }
+// loops
+let numbers = [8, 9, 3, 12, 34];
+// for in loop
+for(let numb in numbers) {
+    console.log(numb)
+};
+console.table(numbers);
+
+let people = {
+    name: 'Cassidy',
+    surname: 'Lawrence',
+    email: 'Cassidy@gmail.com',
+};
+// for(let p in people){
+//     console.log('${p} => ${people[p]}');
+// }
+
+// while loop
+// let cnt = 0;
+// while(cnt < 10) {
+//     console.log('Step: ${cnt}');
+// }
+
+// using numbers array
+// let cnt = 0;
+// while(cnt < numbers.length){
+//     console.log('Step: ${numbers[cnt]}');
+//     cnt++;
+// }
+
+// do while
+// do{
+//     console.log('Step: ${numbers[cnt]}');
+//     cnt++;
+// }while(cnt < numbers.length);
+
+// for of is used to loop through an array. for in is intended for iterating over object keys, not array indexes
+// While loops allows code to execute until its truth conditions are met, this uses boolean do-while loops executes first then checks conditions.
+
+// function
+function addition(numb1, numb2) {
+    console.log("Sum is: ", numb1 + numb2);
+}
+addition(4, 5);
+addition(5, 7);
+addition(9, 23);
+
+// function task
+function subtraction(numb1, numb2) {
+    return numb1 - numb2;
+}
+console.log(subtraction(7, 4));
+
+// rest operator
+function sumOf(...args) {
+    return args.reduce((a, b)=>{return a + b})
+}
+console.log(sumOf(1, 2, 3, 4, 5));
+
+// constructor function
+function Person(...details){
+    this.firstName = details[0];
+    this.lastName = details[1]; 
+    this.email = details.at(-1);
+
+}
+let person1 = new Person("Peter", "Henk", "peter@gmail.com")
+console.log(person1)
